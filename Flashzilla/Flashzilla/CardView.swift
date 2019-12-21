184969eb4a71af176e359c75c1f8d1915badf72f
Zach Frew <zachary.frew@ascension-external.org>
2019-12-21 12:52:43 -0600

    Day 89

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+
     @State var isShowingAnswer = false
     @State var offset = CGSize.zero
     
@@ -10,8 +12,20 @@ struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.white)
-                .shadow(radius: 10)
+            .fill(
+                differentiateWithoutColor
+                    ? Color.white
+                    : Color.white
+                        .opacity(1 - Double(abs(offset.width / 50)))
+
+            )
+            .background(
+                differentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? Color.green : Color.red)
+            )
+            .shadow(radius: 10)
 
             VStack {
                 Text(card.prompt)
