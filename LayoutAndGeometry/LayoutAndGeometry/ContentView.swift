25bf57ac3d365f0be2364cdcd548baae69684c1f
Zach Frew <zachary.frew@ascension-external.org>
2019-12-23 21:46:11 -0600

    Day 91

---
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Text("@just_frew_it")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                Image("background")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("Full name:")
+                Text("ZACHARY FREW")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                    .font(.largeTitle)
+            }
+            
+            VStack {
+                Text("Finn")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutAndGeometry/LayoutAndGeometry/TutorialCode.swift
@@ -0,0 +1,31 @@
+/*
+ struct ContentView: View {
+     var body: some View {
+         HStack(alignment: .lastTextBaseline) {
+             Text("Live")
+                 .font(.caption)
+             Text("long")
+             Text("and")
+                 .font(.title)
+             Text("prosper")
+                 .font(.largeTitle)
+         }
+     }
+ }
+ */
+
+/*
+ struct ContentView: View {
+     var body: some View {
+         VStack(alignment: .leading) {
+             ForEach(0..<10) { position in
+                 Text("Number \(position)")
+                     .alignmentGuide(.leading) { _  in CGFloat(position) * -10 }
+             }
+         }
+         .background(Color.red)
+         .frame(width: 400, height: 400)
+         .background(Color.blue)
+     }
+ }
+ */
